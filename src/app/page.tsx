529c6cbd40d57ded31c40dc797684dde2cf36090
sirguilherme97 <sir_guilherme@hotmail.com>
2023-05-22 01:16:38 -0300

    Codigo comentado + hover btn Pontos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,35 +2,38 @@
 import { useEffect, useState } from 'react';
 import { AiFillPlayCircle, AiFillPauseCircle } from "react-icons/ai";
 import { VscDebugRestart } from "react-icons/vsc";
+
 export default function Home() {
-  const [seconds, setSeconds] = useState(300);
+  const [seconds, setSeconds] = useState(300); // Estado para controlar o tempo em segundos
   const [isPaused, setIsPaused] = useState(true); // Estado para controlar a pausa do timer
-  const [pontosA, setPontosA] = useState(0)
-  const [pontosB, setPontosB] = useState(0)
-  const [timeAName, setTimeAName] = useState("Time A");
-  const [timeBName, setTimeBName] = useState("Time B");
+  const [pontosA, setPontosA] = useState(0); // Estado para armazenar os pontos do Time A
+  const [pontosB, setPontosB] = useState(0); // Estado para armazenar os pontos do Time B
+  const [timeAName, setTimeAName] = useState("Time A"); // Estado para armazenar o nome do Time A
+  const [timeBName, setTimeBName] = useState("Time B"); // Estado para armazenar o nome do Time B
 
-
+  // Funções para adicionar pontos ao Time A
   function somarTresA() {
-    setPontosA(pontosA + 3)
+    setPontosA(pontosA + 3);
   }
   function somarDoisA() {
-    setPontosA(pontosA + 2)
+    setPontosA(pontosA + 2);
   }
   function somarUmA() {
-    setPontosA(pontosA + 1)
+    setPontosA(pontosA + 1);
   }
+
+  // Funções para adicionar pontos ao Time B
   function somarTresB() {
-    setPontosB(pontosB + 3)
+    setPontosB(pontosB + 3);
   }
   function somarDoisB() {
-    setPontosB(pontosB + 2)
+    setPontosB(pontosB + 2);
   }
   function somarUmB() {
-    setPontosB(pontosB + 1)
+    setPontosB(pontosB + 1);
   }
 
-
+  // useEffect para carregar os valores salvos dos cookies ao carregar a página
   useEffect(() => {
     const savedPontosA = parseInt(getCookie('pontosA'));
     const savedPontosB = parseInt(getCookie('pontosB'));
@@ -54,18 +57,22 @@ export default function Home() {
     }
   }, []);
 
+  // useEffect para salvar o valor dos pontos do Time A no cookie sempre que ele for alterado
   useEffect(() => {
     setCookie('pontosA', pontosA.toString() || '', 14);
   }, [pontosA]);
 
+  // useEffect para salvar o valor dos pontos do Time B no cookie sempre que ele for alterado
   useEffect(() => {
     setCookie('pontosB', pontosB.toString() || '', 14);
   }, [pontosB]);
 
+  // useEffect para salvar o nome do Time A no cookie sempre que ele for alterado
   useEffect(() => {
     setCookie('timeAName', timeAName, 14);
   }, [timeAName]);
 
+  // useEffect para salvar o nome do Time B no cookie sempre que ele for alterado
   useEffect(() => {
     setCookie('timeBName', timeBName, 14);
   }, [timeBName]);
@@ -84,16 +91,17 @@ export default function Home() {
     document.cookie = name + "=" + value + "; " + expires;
   };
 
+  // Funções para reiniciar os valores do Time A e Time B
   function handleRestartA() {
     setTimeAName("Time A");
     setPontosA(0);
-  };
+  }
   function handleRestartB() {
     setTimeBName("Time B");
     setPontosB(0);
-  };
+  }
 
-
+  // useEffect para controlar a contagem regressiva do timer
   useEffect(() => {
     if (!isPaused && seconds > 0) { // Verifica se o timer não está em pausa e ainda há segundos restantes
       const timer = setTimeout(() => {
@@ -104,31 +112,33 @@ export default function Home() {
     }
   }, [isPaused, seconds]);
 
-  const minutes = Math.floor(seconds / 60);
-  const formattedSeconds = seconds % 60 < 10 ? `0${seconds % 60}` : seconds % 60;
+  const minutes = Math.floor(seconds / 60); // Calcula os minutos restantes
+  const formattedSeconds = seconds % 60 < 10 ? `0${seconds % 60}` : seconds % 60; // Formata os segundos para exibição
 
+  // Função para alternar entre pausar e retomar o timer
   const handleTimerToggle = () => {
     setIsPaused(!isPaused); // Inverte o estado de pausa do timer ao clicar no botão
   };
 
+  // Funções para atualizar o nome do Time A e Time B ao alterar o valor dos inputs
   const handleTimeANameChange = (event: any) => {
     setTimeAName(event.target.value);
   };
-
   const handleTimeBNameChange = (event: any) => {
     setTimeBName(event.target.value);
   };
+
+  // Funções para atualizar o valor do timer e dos pontos do Time A e Time B ao alterar o valor dos inputs
   const handleTimer = (event: any) => {
     setSeconds(event.target.value);
   };
-
   const handlePontosA = (event: any) => {
     setPontosA(event.target.value);
   };
-
   const handlePontosB = (event: any) => {
     setPontosB(event.target.value);
   };
+
   return (
     <main className="bg-zinc-900 text-zinc-50 w-screen h-full">
       <div className="w-screen h-40 flex items-center justify-center">
@@ -149,13 +159,13 @@ export default function Home() {
           </div>
 
           <div className="flex w-full h-14 text-zinc-900 bg-zinc-900 gap-1">
-            <div className="bg-yellow-500 flex flex-1 items-center justify-center font-bold text-2xl">
+            <div className="bg-yellow-500 active:bg-yellow-700 transition-all flex flex-1 items-center justify-center font-bold text-2xl">
               <p className="w-full h-full flex items-center justify-center" onClick={somarTresA}>+3</p>
             </div>
-            <div className="bg-yellow-500 flex flex-1 items-center justify-center font-bold text-2xl">
+            <div className="bg-yellow-500 active:bg-yellow-700 transition-all flex flex-1 items-center justify-center font-bold text-2xl">
               <p className="w-full h-full flex items-center justify-center" onClick={somarDoisA}>+2</p>
             </div>
-            <div className="bg-yellow-500 flex flex-1 items-center justify-center font-bold text-2xl">
+            <div className="bg-yellow-500 active:bg-yellow-700 transition-all flex flex-1 items-center justify-center font-bold text-2xl">
               <p className="w-full h-full flex items-center justify-center" onClick={somarUmA}>+1</p>
             </div>
           </div>
@@ -174,13 +184,13 @@ export default function Home() {
           </div>
 
           <div className="flex w-full h-14 text-zinc-900 bg-zinc-900 gap-1">
-            <div className="bg-yellow-500 flex flex-1 items-center justify-center font-bold text-2xl">
+            <div className="bg-yellow-500 active:bg-yellow-700 transition-all flex flex-1 items-center justify-center font-bold text-2xl">
               <p className="w-full h-full flex items-center justify-center" onClick={somarTresB}>+3</p>
             </div>
-            <div className="bg-yellow-500 flex flex-1 items-center justify-center font-bold text-2xl">
+            <div className="bg-yellow-500 active:bg-yellow-700 transition-all flex flex-1 items-center justify-center font-bold text-2xl">
               <p className="w-full h-full flex items-center justify-center" onClick={somarDoisB}>+2</p>
             </div>
-            <div className="bg-yellow-500 flex flex-1 items-center justify-center font-bold text-2xl">
+            <div className="bg-yellow-500 active:bg-yellow-700 transition-all flex flex-1 items-center justify-center font-bold text-2xl">
               <p className="w-full h-full flex items-center justify-center" onClick={somarUmB}>+1</p>
             </div>
           </div>
@@ -189,7 +199,7 @@ export default function Home() {
 
       <div className='w-full h-auto mt-10 flex items-center gap-10 justify-center'>
         {isPaused ? (
-          <div className='flex flex-col pt-10 items-center justify-center'>
+          <div className='flex mb-10 flex-col pt-10 items-center justify-center'>
             <AiFillPlayCircle size={120} onClick={handleTimerToggle} />
             <div className=''>
               <p className='mt-10 '>Tempo de Jogo - SEGUNDOS</p>
@@ -200,21 +210,21 @@ export default function Home() {
                 onChange={handleTimer}
                 className="rounded-md placeholder:text-zinc-500 placeholder:text-lg pl-5 mt-2 w-full h-10 bg-zinc-800 font-bold text-2xl flex items-center justify-center"
               />
-              <p className='mt-4 '>{`Pontos: ${timeAName}`}</p>
+              <p className='mt-10'>Pontos do Time A</p>
               <input
-                placeholder='Pontos'
+                placeholder='Pontos do Time A'
                 type="number"
                 value={pontosA}
                 onChange={handlePontosA}
-                className="rounded-md placeholder:text-zinc-50 placeholder:text-lg  pl-5 mt-2 w-full h-10 bg-zinc-800 font-bold text-2xl flex items-center justify-center"
+                className="rounded-md placeholder:text-zinc-500 placeholder:text-lg pl-5 mt-2 w-full h-10 bg-zinc-800 font-bold text-2xl flex items-center justify-center"
               />
-              <p className='mt-4 '>{`Pontos: ${timeBName}`}</p>
+              <p className='mt-10'>Pontos do Time B</p>
               <input
-                placeholder='Pontos'
+                placeholder='Pontos do Time B'
                 type="number"
                 value={pontosB}
                 onChange={handlePontosB}
-                className="rounded-md mb-20 placeholder:text-zinc-50 placeholder:text-lg pl-5 mt-2 w-full h-10 bg-zinc-800 font-bold text-2xl flex items-center justify-center"
+                className="rounded-md placeholder:text-zinc-500 placeholder:text-lg pl-5 mt-2 w-full h-10 bg-zinc-800 font-bold text-2xl flex items-center justify-center"
               />
             </div>
           </div>
@@ -224,8 +234,6 @@ export default function Home() {
           </div>
         )}
       </div>
-
-
     </main>
   );
 }
